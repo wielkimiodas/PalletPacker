8c70c69a6eda60032e2d94ac7ef10b8a02a03189
Tomek <webtom90@gmail.com>
2014-01-15 15:50:13 +0100

    p1 - porównywanie błędów w zbiorze treningowym

--- octave project/licz.m
+++ octave project/licz.m
@@ -17,26 +17,43 @@ w=zeros(size(X,2),1);
 
 for it=1:10,
 	w=reglog(X, Y, w, lambda);
-	blad01 = 0;
-	bladLog = 0;
+	blad01Test = 0;
+	bladLogTest = 0;
 	for i=1:size(XT, 1),
 		realY = XT(i, :) * w;
-		bladLog = bladLog + log(1 + exp(-YT(i) * (w' * XT(i, :)')));
+		bladLogTest = bladLogTest + log(1 + exp(-YT(i) * (w' * XT(i, :)')));
 		if (realY >= 0)
 			if (YT(i) < 0)
-				blad01 = blad01 + 1;
+				blad01Test = blad01Test + 1;
 			endif
 		else
 			if (YT(i) >= 0)
-				blad01 = blad01 + 1;
+				blad01Test = blad01Test + 1;
 			endif
 		endif
 	end
-		
-	bladLog = bladLog / size(XT, 1);
-	blad01 = blad01 / size(XT, 1);
+	bladLogTest = bladLogTest / size(XT, 1);
+	blad01Test = blad01Test / size(XT, 1);
+	
+	blad01Train = 0;
+	bladLogTrain = 0;
+	for i=1:size(X, 1),
+		realY = X(i, :) * w;
+		bladLogTrain = bladLogTrain + log(1 + exp(-Y(i) * (w' * X(i, :)')));
+		if (realY >= 0)
+			if (Y(i) < 0)
+				blad01Train = blad01Train + 1;
+			endif
+		else
+			if (Y(i) >= 0)
+				blad01Train = blad01Train + 1;
+			endif
+		endif
+	end
+	bladLogTrain = bladLogTrain / size(X, 1);
+	blad01Train = blad01Train / size(X, 1);
 
-	disp([it bladLog blad01])
+	disp([it bladLogTrain blad01Train bladLogTest blad01Test])
 end
 toc
 
@@ -49,27 +66,45 @@ for it=0:2000,
 	alpha = c / sqrt(it + 1);
 	w = stochgrad(X, Y, w, alpha);
 	
-	if (mod(it, 200) == 0),
-		blad01 = 0;
-		bladLog = 0;
+	%if (mod(it, 200) == 0),
+	if (mod(it, 1) == 0),
+		blad01Test = 0;
+		bladLogTest = 0;
 		for i=1:size(XT, 1),
 			realY = XT(i, :) * w;
-			bladLog = bladLog + log(1 + exp(-YT(i) * (w' * XT(i, :)')));
+			bladLogTest = bladLogTest + log(1 + exp(-YT(i) * (w' * XT(i, :)')));
 			if (realY >= 0)
 				if (YT(i) < 0)
-					blad01 = blad01 + 1;
+					blad01Test = blad01Test + 1;
 				endif
 			else
 				if (YT(i) >= 0)
-					blad01 = blad01 + 1;
+					blad01Test = blad01Test + 1;
 				endif
 			endif
 		end
+		bladLogTest = bladLogTest / size(XT, 1);
+		blad01Test = blad01Test / size(XT, 1);
 		
-		bladLog = bladLog / size(XT, 1);
-		blad01 = blad01 / size(XT, 1);
+		blad01Train = 0;
+		bladLogTrain = 0;
+		for i=1:size(X, 1),
+			realY = X(i, :) * w;
+			bladLogTrain = bladLogTrain + log(1 + exp(-Y(i) * (w' * X(i, :)')));
+			if (realY >= 0)
+				if (Y(i) < 0)
+					blad01Train = blad01Train + 1;
+				endif
+			else
+				if (Y(i) >= 0)
+					blad01Train = blad01Train + 1;
+				endif
+			endif
+		end
+		bladLogTrain = bladLogTrain / size(X, 1);
+		blad01Train = blad01Train / size(X, 1);
 
-		disp([it bladLog blad01])
+		disp([it bladLogTrain blad01Train bladLogTest blad01Test])
 	endif
 end
 toc
